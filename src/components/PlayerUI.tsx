0639ad0bf4f26544d4678c66c3828a2ff1c6290c
Ray Blaak <blaak@ensemble.com>
2024-04-05 15:22:39 -0700

    WIP

--- src/components/PlayerUI.tsx
+++ src/components/PlayerUI.tsx
@@ -1,9 +1,9 @@
-import React, {useState} from 'react';
+import React from 'react';
 import {Platform, StyleSheet, Text, View,} from 'react-native';
-import {VideoPlayer} from '@amzn/react-native-w3cmedia';
 import BackButton from './BackButton';
 
 import PlaybackControls, {seekBackward, seekForward, VideoPlayerRef} from './PlaybackControls';
+import {HWEvent, useTVEventHandler} from "@amzn/react-native-kepler";
 
 interface PlayerUIProps {
   videoRef: VideoPlayerRef;
@@ -16,7 +16,7 @@ interface Header {
   navigateBack: () => void;
 }
 
-const Header = ({ title, navigateBack }: Header) => {
+const Header = ({title, navigateBack}: Header) => {
   return (
     <View style={styles.header}>
       <BackButton onPress={navigateBack} hasTVPreferredFocus={true}/>
@@ -29,84 +29,40 @@ const Header = ({ title, navigateBack }: Header) => {
 
 
 export const PlayerUI = ({
-  navigateBack,
-  title,
-  videoRef,
-}: PlayerUIProps) => {
-  const [captionMenuVisibility, setCaptionMenuVisibility] = useState(false);
+                           navigateBack,
+                           title,
+                           videoRef,
+                         }: PlayerUIProps) => {
   if (Platform.isTV) {
     useTVEventHandler((evt: HWEvent) => {
       if (evt && evt.eventKeyAction === 0 && videoRef.current) {
-        let playbackEvent = null;
-        try {
-          if (evt.eventType === 'playpause') {
-            console.log("k_content_per: PlayerUI : playpause");
-            if (videoRef.current.paused) {
-              videoRef.current.play();
-              playbackEvent = getMockPlaybackEventForVideo(videoRef, title, PlaybackState.PLAYING);
-
-            } else {
-              videoRef.current.pause();
-              playbackEvent = getMockPlaybackEventForVideo(videoRef, title, PlaybackState.PAUSED);
-            }
-          } else if (evt.eventType === 'play') {
-            console.log("k_content_per: PlayerUI : play");
+        if (evt.eventType === 'playpause') {
+          if (videoRef.current.paused) {
             videoRef.current.play();
-            playbackEvent = getMockPlaybackEventForVideo(videoRef, title, PlaybackState.PLAYING);
-          } else if (evt.eventType === 'pause') {
-            console.log("k_content_per: PlayerUI : pause");
+          } else {
             videoRef.current.pause();
-            playbackEvent = getMockPlaybackEventForVideo(videoRef, title, PlaybackState.PAUSED);
-          } else if (evt.eventType === 'skip_forward') {
-            console.log("k_content_per: PlayerUI : skip_forward");
-            seekForward(videoRef);
-            playbackEvent = getMockPlaybackEventForVideo(videoRef, title, PlaybackState.PLAYING);
-          } else if (evt.eventType === 'skip_backward') {
-            console.log("k_content_per: PlayerUI : skip_backward");
-            seekBackward(videoRef);
-            playbackEvent = getMockPlaybackEventForVideo(videoRef, title, PlaybackState.PLAYING);
           }
-        }
-        catch (e) {
-          console.error(`k_content_per: ${e}`);
-      }
-        if (playbackEvent) {
-          console.log(`k_content_per: useTvEventHandler: Reporting new playback event : ${JSON.stringify(playbackEvent)}`);
-          ContentPersonalizationServer.reportNewPlaybackEvent(playbackEvent);
+        } else if (evt.eventType === 'play') {
+          videoRef.current.play();
+        } else if (evt.eventType === 'pause') {
+          videoRef.current.pause();
+        } else if (evt.eventType === 'skip_forward') {
+          seekForward(videoRef);
+        } else if (evt.eventType === 'skip_backward') {
+          seekBackward(videoRef);
         }
       }
     });
   }
-
-  const toggleCaption = () => {
-    setCaptionMenuVisibility(!captionMenuVisibility);
-  };
-
-  const ControlBarMenu = () => {
-    return (
-      <View style={styles.playerMenusContainer}>
-        <CaptionMenu
-          captionMenuVisibility={captionMenuVisibility}
-          video={videoRef.current as VideoPlayer}
-        />
-      </View>
-    );
-  };
   return (
     <View style={styles.uiContainer}>
       <View style={styles.ui}>
-        <Header navigateBack={navigateBack} title={title} />
-        <PlaybackControls videoRef={videoRef} />
-        <ControlBar
-          videoRef={videoRef}
-          captions={toggleCaption}
-          captionMenuVisibility={captionMenuVisibility}
-        />
+        <Header navigateBack={navigateBack} title={title}/>
+        <PlaybackControls videoRef={videoRef}/>
       </View>
-      <ControlBarMenu />
     </View>
   );
-};
+}
 
 const styles = StyleSheet.create({
   playerMenusContainer: {
@@ -127,7 +83,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   title: {
-    color: COLORS.WHITE,
+    color: 'white',
     fontWeight: '500',
     fontSize: 70,
     marginLeft: 90,
