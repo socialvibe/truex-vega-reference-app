994a0c62028b560a34989a3348fdacb35133b9cf
Ray Blaak <blaak@ensemble.com>
2024-04-16 11:38:55 -0700

    WIP: cleanup

--- metro.config.js
+++ metro.config.js
@@ -1,17 +1,21 @@
-/*
- * Copyright (c) 2022 Amazon.com, Inc. or its affiliates.  All rights reserved.
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+const path = require('node:path');
+
+/**
+ + * Metro configuration
+ + * https://facebook.github.io/metro/docs/configuration
  *
- * PROPRIETARY/CONFIDENTIAL.  USE IS SUBJECT TO LICENSE TERMS.
+ + * @type {import("metro-config").MetroConfig}
  */
+const config = {
+  // These attempts to allow for local lib builds for dev/debug result in simulator crashes
+  // watchFolders: [
+  //   path.resolve(__dirname),
+  //   path.resolve(__dirname, '../TruexAdRenderer-Kepler')
+  // ],
+  // resolver: {
+  //   unstable_enableSymlinks: true // enable local debugging of libs via `npm link`
+  // }
+};
 
-const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
-
- /**
-+ * Metro configuration
-+ * https://facebook.github.io/metro/docs/configuration
-  *
-+ * @type {import('metro-config').MetroConfig}
-  */
-const config = {};
- 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
