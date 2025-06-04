19c6656956e9c244b73a8005d640c3d34a4850e8
Ray Blaak <blaak@ensemble.com>
2025-06-04 15:16:53 -0700

    back handler explorations

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,8 @@ export const App = () => {
   const webSource = useMemo(() => {
     //const url = 'https://ctv.truex.com/kepler/test/test-page.html?cb=' + Date.now();
     //const url = "https://qa-media.truex.com/container/3.x/current/ctv.html#creative_json_url=https%3A%2F%2Fqa-ee.truex.com%2Fstudio%2Fdrafts%2F5179%2Fconfig_json&session_id=100cfe41-7638-4e32-9383-4f0a1e6eebde&multivariate%5Bctv_footer_test%5D=T0&multivariate%5Bctv_relevance_enabled%5D=true";
-    const url = "https://ctv.truex.com/android/bridge/v2/release/index.html?test=1";
+    //const url = "https://ctv.truex.com/android/bridge/v2/release/index.html?test=1";
+    const url = "https://ctv.truex.com/android/bridge/v2/qa/index.html?test_vast_config_url=qa-get.truex.com%2Fc39e2b60633fcda48cbbc60b9628af64cf23ff9d%2Fvast%2Fconfig%3Fdimension_1%3DPI-2447-C3-ctv-ad";
     return { uri: url }
   }, []);
 
