bf5421091e35ec7705e8c027f3f3cf66acf4e3be
Ray Blaak <blaak@ensemble.com>
2025-06-05 12:22:26 -0700

    demonstrate console logging bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,71 +1,17 @@
-import React, { useCallback, useEffect, useMemo, useState } from 'react';
-import 'react-native-gesture-handler';
-import { StyleSheet, View } from 'react-native';
-import { BackHandler } from '@amzn/react-native-kepler';
-import uuid from 'react-native-uuid';
-import { TruexAdOptions } from './truex/TruexAdOptions';
-import { VideoStreamConfig } from './video/VideoStreamConfig';
-import videoStreamJson from './data/video-stream.json';
-import { AdEventHandler, TruexAdEvent } from './truex/TruexAdEvent';
-import TruexAd from './truex/TruexAd';
-
-const videoStream = videoStreamJson as VideoStreamConfig;
+import React, { useEffect } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
 export const App = () => {
-  const [showTruexAd, setShowTruexAd] = useState(true);
-
-  const onAdEvent: AdEventHandler = useCallback<AdEventHandler>(
-    (event, data) => {
-      console.log(`*** truex event: ${event}`);
-      switch (event) {
-        case TruexAdEvent.AdFreePod:
-          // Remember for later that we have the ad credit.
-          //hasAdCredit.current = true;
-          break;
-
-        case TruexAdEvent.AdCompleted:
-        case TruexAdEvent.AdError:
-        case TruexAdEvent.NoAdsAvailable:
-          // Resume playback.
-          //play();
-          // if (hasAdCredit.current && afterAdResumeTarget.current !== undefined) {
-          //   // Skip over the fallback ads.
-          //   seekTo(afterAdResumeTarget.current);
-          // }
-          setShowTruexAd(false);
-
-          // ensure our page has the focus again
-          // @TODO does not seem to work however, we are still losing keyboard focus after webview unmounts
-          //pageRef.current?.focus();
-          break;
-      }
-    },
-    []
-  );
-
   useEffect(() => {
-    const onBackHandler = () => {
-      if (showTruexAd) return false; // let the truex ad handle the back action
-      BackHandler.exitApp();
-      return true; // handled
-    };
-    BackHandler.addEventListener('hardwareBackPress', onBackHandler);
-    return () => BackHandler.removeEventListener('hardwareBackPress', onBackHandler);
-  }, [showTruexAd]);
-
-  const tarOptions = useMemo<TruexAdOptions>(() => {
-    const options: TruexAdOptions = {
-      // Ensure a unique user id to minimize no ads available
-      userAdvertisingId: uuid.v4() as string
-    };
-    return options;
+    console.log("*** test log: this should show only once");
+    console.info("*** test info: this should show only once");
+    console.warn("*** test warn: this should show only once");
+    console.error("*** test error: this should show only once");
   }, []);
 
   return (
     <View style={styles.app}>
-      {showTruexAd && (
-        <TruexAd vastConfigUrl={videoStream.adBreaks[0].vastUrl} options={tarOptions} onAdEvent={onAdEvent} />
-      )}
+      <Text style={styles.text}>Hello world!</Text>
     </View>
   );
 };
@@ -75,8 +21,12 @@ const styles = StyleSheet.create({
     margin: 0,
     width: '100%',
     height: '100%',
-    backgroundColor: '#2222CC',
+    backgroundColor: '#444444',
     display: 'flex',
     flex: 1
+  },
+  text: {
+    fontSize: 22,
+    color: 'white'
   }
 });
